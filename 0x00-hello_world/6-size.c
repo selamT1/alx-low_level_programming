74273a1051e2a03dace3b309d15ed04e328645e8
selamT1 <solla.tsg14@gmail.com>
2022-12-09 07:09:06 -0800

    update the file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+/**
+ * main- main function is the entery point of the code
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	printf("the date size of integer is %d\n", sizeof(x));
+	return (0);
+
+}
