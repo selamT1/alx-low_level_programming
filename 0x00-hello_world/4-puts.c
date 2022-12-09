bd7a958128c29ccbfa7f355edf6d64676550efac
selamT1 <solla.tsg14@gmail.com>
2022-12-08 19:29:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main-main block
- * return: 0
+ * return: Always 0 (Success)
  */
 int main(void)
 {
 /*this is main function with no return value*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return 0;
 }
