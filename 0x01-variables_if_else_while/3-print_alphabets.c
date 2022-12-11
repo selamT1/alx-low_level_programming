e3820f212a19efd0d0fc0316a5274e088f7538bc
selamT1 <solla.tsg14@gmail.com>
2022-12-11 11:14:33 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -while loop
+ * description: print capital and small letters
+ * Return: 0
+ */
+int main(void)
+{
+	char lc = 'a';
+	char UC = 'A';
+
+	while (ls <= 'z')
+	{
+		putchar(ls);
+		ls++;
+	}
+	while (UC <= 'Z')
+	{
+		putchar(UC);
+		UC++;
+	}
+	putchar('\n');
+}
