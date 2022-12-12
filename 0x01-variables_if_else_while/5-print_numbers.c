4135273a2b4b7ff8ca2c9ced9f008afaa5de5d99
selamT1 <solla.tsg14@gmail.com>
2022-12-11 17:56:31 -0800

    5.numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-function
+ * description: print no
+ * Return:0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
