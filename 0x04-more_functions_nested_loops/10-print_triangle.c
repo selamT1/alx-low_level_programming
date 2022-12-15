68865a207af88bb78a6eb2334b3ea468e5d981fb
selamT1 <solla.tsg14@gmail.com>
2022-12-15 15:40:45 -0800

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,11 +6,11 @@
 void print_triangle(int size)
 {
 	int i, j, a;
-	int count = size - 1;
+	int count = size;
 
-	for (i = 0; i <= size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < count; j++)
+		for (j = 1; j < count; j++)
 		{
 			_putchar(32);
 		}
