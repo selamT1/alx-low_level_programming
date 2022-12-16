6dfa730aac469be4707ab11a207aadacffb41f39
selamT1 <solla.tsg14@gmail.com>
2022-12-15 23:39:47 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,21 @@ int main(void)
 	for (i = 1; i <= n; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else if ((i % 3) == 0)
+		{
 			printf("Fizz ");
+		}
 		else if ((i % 5) == 0)
+		{
 			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("\n");
 	return (0);
