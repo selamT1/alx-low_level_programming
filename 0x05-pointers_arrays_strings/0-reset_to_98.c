a54fe93d25c5fd991290faf6ba1eb294c826d3c7
selamT1 <solla.tsg14@gmail.com>
2022-12-19 12:00:38 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * void reset_to_98 - converts pointer address into values
+ * @n: address of value
+ * Return: empty
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is project on arrary pointer and strings
