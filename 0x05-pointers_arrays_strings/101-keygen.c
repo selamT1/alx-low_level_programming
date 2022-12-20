15cda3c9608b18979bcd99426787b078222cb62e
selamT1 <solla.tsg14@gmail.com>
2022-12-20 04:53:14 -0800

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,22 +5,18 @@
  * main - print randdom password
  * Return:integer
  */
-
-int main(void)                                                                                                                     
+int main(void)
 {
 	int i, sum, n;
 	int pass[100];
 
 	sum = 0;
-
 	srand(time(NULL));
-	
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
@@ -29,4 +25,5 @@ int main(void)
 			break;
 		}
 	}
+	return (0);
 }
