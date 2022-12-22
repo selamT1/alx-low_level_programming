861ad069facb4b92b6d007b335cbe44f0760a322
selamT1 <solla.tsg14@gmail.com>
2022-12-22 11:15:39 -0800

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,11 +8,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int len = 0, i = 0;
 
-	for (i = 0; i < n; i++)
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; src[i] && i < n; i++)
 	{
 		dest[i] = src[i];
+	}
+	for (i = len; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
