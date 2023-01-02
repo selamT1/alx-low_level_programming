e9de1fb260b171249107b92b2deda5b2d40b480f
selamT1 <solla.tsg14@gmail.com>
2023-01-02 11:29:35 -0800

    memset update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,14 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	     int i;
-	     i = 0;
-	     
-	     while (i < n)
-	     {
-		     s[i] = b;
-		     i++;
-	     }
-	     return (s);
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
 
