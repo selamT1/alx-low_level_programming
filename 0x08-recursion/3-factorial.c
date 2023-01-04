0cb1a69e440799c91a62a1d0ee761fc3c27d6f01
selamT1 <solla.tsg14@gmail.com>
2023-01-04 14:16:37 -0800

    update

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int factorial - return the factorial of the gien number
+ * factorial - return the factorial of the gien number
  * @n: accepted parameter
  * Return: -1 if n is negative int, a if n is 0, otherwise fact.value
  */
