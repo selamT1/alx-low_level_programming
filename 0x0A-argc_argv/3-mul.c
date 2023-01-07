8fa413a7acf334b014e48bd14ce5fe3036803e21
selamT1 <solla.tsg14@gmail.com>
2023-01-06 16:51:32 -0800

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - converts string to int
+ * @s: string pointer
+ * Return: int value
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			pn *= -1;
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: arg size
+ * @argv: arg pointer
+ * Return: 0 if correct or 1 if an (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
+
