0818842fc786f600cc2b1c8d04754a7af997541b
selamT1 <solla.tsg14@gmail.com>
2023-01-16 11:05:04 -0800

    0.ppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+/**
+ * struct dog - a dog's list
+ * @name: char type
+ * @age: float type
+ * @owner: pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char * owner;
+};
+
+#endif
