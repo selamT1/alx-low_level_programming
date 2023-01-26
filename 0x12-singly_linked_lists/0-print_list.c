39e54b6f8ee38caa2b70f6f9265a8cfe523b9d9b
selamT1 <solla.tsg14@gmail.com>
2023-01-26 15:01:10 -0800

    readme list header and task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: pointer to the list_t list to print
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
