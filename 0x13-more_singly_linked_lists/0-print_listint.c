b04026489ac50cc5523f6a7095a4767d1b1f9786
selamT1 <solla.tsg14@gmail.com>
2023-01-31 22:47:37 +0000

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (h == NULL)
-			printf("null pointer\n");
-		else
-			printf("%u\n", h->n);
+		printf("%u\n", h->n);
 		h = h->next;
 		count++;
 	}
