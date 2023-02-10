500a47d46e5803293badae0185f75923dbd540f5
selamT1 <solla.tsg14@gmail.com>
2023-02-10 12:11:47 -0800

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * create_file - Creates a file.
  * @filename: file name pointer.
- * @text_content: a string pointer.
- *
- * Return: If file name is NULL return -1, if text empty file create empty file.
+ * @text_content: a string pointer
+ * Return: If file name is NULL return -1
+ * if text empty file create empty file.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -13,21 +12,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
-
 	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	j = write(i, text_content, len);
-
 	if (i == -1 || j == -1)
 		return (-1);
-
 	close(i);
-
 	return (1);
 }
 
